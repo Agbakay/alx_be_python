3733229842bae8338ef6b6c133e789ce6c195cdc
Akintunde Kayode <kayodeakintunde919@gmail.com>
2025-09-21 12:22:12 +0100

    New commit

--- control-flow/weather_advice.py
+++
@@ -1,9 +0,0 @@
-weather = input("What's the weather like today? (sunny, rainny, cold): " )
-if weather == 'sunny':
-    print("Wear a t-shirt and sunglasses")
-elif weather == 'rainny':
-        print("Don't forget your umbrella and a raincoat")
-elif weather == 'cold':
-            print(" Sorry, I don't have recommendations for this weather.")
-else:
-        print(" Sorry, I don't have recommendations for this weather.")
