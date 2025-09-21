cc030df657d6deed4c387c2892e3dfc2d6cecf7f
Akintunde Kayode <kayodeakintunde919@gmail.com>
2025-09-21 15:42:50 +0100

    New Commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sumval = number1+number2
+diff = number1-number2
+mult = number1*number2
+print(f"Addition of {number1} and {number2} is {sumval}")
+print(f"Subtraction of {number1} and {number2} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {mult}")
